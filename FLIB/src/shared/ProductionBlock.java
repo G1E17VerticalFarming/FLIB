b30ac946be51355f98618671d5ff3ebe511206dc
agger1995 <christian_agger1995@hotmail.com>
2017-10-11 11:50:35 +0200

    Simple API for MES and SCADA complete. Added common classes to library. Moved IGreenhouse to common library.

--- FLIB/src/shared/ProductionBlock.java
+++ FLIB/src/shared/ProductionBlock.java
@@ -1,15 +1,21 @@
-package interfaces;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package shared;
+
+import java.util.Date;
 
 /**
  *
  * @author chris
  */
-public interface IScadaFacade {
+public class ProductionBlock {
+    private int status;
+    private int batchId;
+    private Date estimatedDone;
+    private int growthConfigId;
+    private int plantType;
     
 }
